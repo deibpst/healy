b174446acbed2e19277aa105507049d1c07ceb6b
Joss-Y8 <romero080505@gmail.com>
2025-11-19 12:49:21 -0600

    Botón deashboard paciente

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -60,6 +60,13 @@ export default function Home() {
 					>
 						Calendario
 					</Link>
+					<Link
+						className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-zinc-300 bg-white px-5 transition-colors hover:bg-gray-100 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+						href="patient-dashboard"
+						rel="noopener noreferrer"
+					>
+						Patient Dashboard
+					</Link>
 				</div>
 			</main>
 		</div>
