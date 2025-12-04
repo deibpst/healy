15d37d53758b233fa25241dd72e81d4bc8d61950
David Tipa <hegbj.jegh15@gmail.com>
2025-12-03 22:26:03 -0600

    cambio en la forma del dashboard

--- frontend/app/(auth)/components/patient/NextAppointment.tsx
+++ frontend/app/(auth)/components/patient/NextAppointment.tsx
@@ -54,9 +54,7 @@ const NextAppointment = () => {
         </div>
 
         <div className="flex gap-2 pt-2">
-          <Button variant="outline" className="flex-1">
-            Ver Detalles
-          </Button>
+          
           <Button className="flex-1">
             Agregar a Calendario
           </Button>
